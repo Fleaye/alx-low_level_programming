59508c4d6acb3104bb7a802dd5a842c8481069cd
Filagot Berhanu <fleabeye@gmail.com>
2023-06-15 08:58:10 -0700

    my fourth c project called 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <std10.h>
 /**
- * main - a programing is like building a multilingual puzzle
+ * main - A programing that print a line puts function
  * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
