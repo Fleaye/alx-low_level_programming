fac3c962b9c6afe9632c3b9955539374cbd10b49
Filagot Berhanu <fleabeye@gmail.com>
2023-06-15 08:46:28 -0700

    fourth project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <std10.h>
+/**
+ * main - a programing is like building a multilingual puzzle
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"");
+return (0);
+}
