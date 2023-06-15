417e1769c4cb7ed21a98b401c6ef7b2bdd6563ed
Filagot Berhanu <fleabeye@gmail.com>
2023-06-15 10:53:14 -0700

    fourth project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdi0.h>
+#include<stdi0.h>
 /**
- * main - A programing that print a line puts function
- * Return: 0 (success)
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
