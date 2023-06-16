f9899bbb7f63b209c9d590e144d62c487b699487
Filagot Berhanu <fleabeye@gmail.com>
2023-06-16 11:38:23 -0700

    c varaible project posetive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is posetive, negative or zero.
+ * main - Determines if a number is positive, negative or zero.
  *
  * Return: Always 0 (Success)
  */
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is posetive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -26,4 +26,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}			
+}
